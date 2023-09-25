f3189c928cdc9c1c5c04e452347f653965b23cd2
truffet <truffet@student.42.fr>
2023-09-25 14:18:01 +0200

    Update python-class-example.py
    added class inheritance in the example

--- python-class-example.py
+++ python-class-example.py
@@ -1,3 +1,4 @@
+# parent class
 class NewObjectInstance:
 	x = 0
 
@@ -7,14 +8,25 @@ class NewObjectInstance:
 
 	def NewMethod(self):
 		self.x = self.x + 1
-		print(f"Object Method was used {self.x} times.")
+		print(f"Object Method from parent class was used {self.x} times.")
 
 	def __del__(self):
 		print(f"Object {self.name} was destructed.")
 
+# child class
+class NewObjectInheritedInstance(NewObjectInstance):
+	y = 0
+
+	def NewMethod2(self):
+		self.y = self.y + 1
+		print(f"Object Method from child class was used {self.y} times.")
+
 instance1 = NewObjectInstance("instance 1")
 instance2 = NewObjectInstance("instance 2")
+instance3 = NewObjectInheritedInstance("instance 3")
 instance1.NewMethod()
 instance1.NewMethod()
 instance2.NewMethod()
 instance2.NewMethod()
+instance3.NewMethod()
+instance3.NewMethod2()
